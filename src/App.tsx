e76783d77356917a553d4e47144d0d20d2f2f421
HalloJo <jorikvanruiswijk@gmail.com>
2022-11-21 12:58:56 +0100

    Set up Router and Routes, project title and new favicon

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
+import { Navigate, Route, Routes } from "react-router-dom";
 import "./scss/App.scss";
 import "./scss/globals.scss";
 
 const App = () => {
   return (
     <div className="App">
-      <h2>On a more serious note.</h2>
+      <Routes>
+        <Route path="/" element={<h1>On a more serious note.</h1>} />
+        <Route path="/new" element={<p>New</p>} />
+        <Route path="/:id">
+          <Route index element={<h1>Show</h1>} />
+          <Route path="edit" element={<h1>Edit</h1>} />
+        </Route>
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
     </div>
   );
 };
