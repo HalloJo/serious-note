b53feabd3bfd595887dc8d188a71f3b35720adce
HalloJo <jorikvanruiswijk@gmail.com>
2022-11-23 14:36:38 +0100

    Begin styling EditTagsModal

--- src/components/EditTagsModal.tsx
+++ src/components/EditTagsModal.tsx
@@ -1,4 +1,5 @@
 import { Tag } from "../types";
+import styles from "../scss/EditTagsModal.module.scss";
 
 type EditTagsModalProps = {
   availableTags: Tag[];
@@ -16,23 +17,32 @@ const EditTagsModal = ({
   updateTag,
 }: EditTagsModalProps) => {
   return (
-    <div>
-      <h1>Edit tags</h1>
-      <button onClick={() => handleClose(!show)}>Close</button>
-      <form action="">
-        {availableTags.map((tag) => (
-          <div key={tag.id}>
-            <input
-              type="text"
-              name="title"
-              id="title"
-              value={tag.label}
-              onChange={(event) => updateTag(tag.id, event.target.value)}
-            />
-            <button onClick={() => deleteTag(tag.id)}>&times;</button>
-          </div>
-        ))}
-      </form>
+    <div className={styles.modal__background}>
+      <div className={styles.modal__wrapper}>
+        <div className={styles.modal__header}>
+          <h2 className={styles.modal__title}>Edit tags</h2>
+          <button
+            className={styles.modal__closeButton}
+            onClick={() => handleClose(!show)}
+          >
+            &times;
+          </button>
+        </div>
+        <form action="">
+          {availableTags.map((tag) => (
+            <div className={styles.modal__inputWrapper} key={tag.id}>
+              <input
+                type="text"
+                name="title"
+                id="title"
+                value={tag.label}
+                onChange={(event) => updateTag(tag.id, event.target.value)}
+              />
+              <button onClick={() => deleteTag(tag.id)}>&times;</button>
+            </div>
+          ))}
+        </form>
+      </div>
     </div>
   );
 };
