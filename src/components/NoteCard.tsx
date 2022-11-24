fb3aa6d46ebf0682aaa4e749178bcb9de0bbbb84
HalloJo <jorikvanruiswijk@gmail.com>
2022-11-24 11:15:56 +0100

    Style NoteCard and Noteslist

--- src/components/NoteCard.tsx
+++ src/components/NoteCard.tsx
@@ -1,17 +1,22 @@
 import { Link } from "react-router-dom";
 import { SimplifiedNote, Tag } from "../types";
+import styles from "../scss/NoteCard.module.scss";
 
 const NoteCard = ({ id, title, tags }: SimplifiedNote) => {
   return (
-    <Link to={`/${id}`}>
-      <h3>{title}</h3>
-      {tags.length > 0 && (
-        <ul>
-          {tags.map((tag) => (
-            <li key={tag.id}>{tag.label}</li>
-          ))}
-        </ul>
-      )}
+    <Link to={`/${id}`} className={styles.noteCard}>
+      <div className={styles.noteCard__wrapper}>
+        <h3 className={styles.noteCard__title}>{title}</h3>
+        {tags.length > 0 && (
+          <ul className={styles.noteCard__tagList}>
+            {tags.map((tag) => (
+              <li className={styles.noteCard__tag} key={tag.id}>
+                {tag.label}
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
     </Link>
   );
 };
