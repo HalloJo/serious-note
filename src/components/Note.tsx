96eff828b09632b7c50cedac9c4bbcf197378a4f
HalloJo <jorikvanruiswijk@gmail.com>
2022-11-24 14:06:31 +0100

    Style Note component

--- src/components/Note.tsx
+++ src/components/Note.tsx
@@ -1,6 +1,7 @@
 import { Link, useNavigate } from "react-router-dom";
 import { useNote } from "./NotesLayout";
 import ReactMarkdown from "react-markdown";
+import styles from "../scss/Note.module.scss";
 
 type NoteProps = {
   onDelete: (id: string) => void;
@@ -11,35 +12,47 @@ const Note = ({ onDelete }: NoteProps) => {
   const navigate = useNavigate();
 
   return (
-    <>
-      <h1>{note.title}</h1>
-      {note.tags.length > 0 && (
-        <ul>
-          {note.tags.map((tag) => (
-            <li key={tag.id}>{tag.label}</li>
-          ))}
-        </ul>
-      )}
-      <Link to={`/${note.id}/edit`}>
-        <button className="notesList__buttonPrimary" type="button">
-          Edit
-        </button>
-      </Link>
-      <button
-        onClick={() => {
-          onDelete(note.id);
-          navigate("/");
-        }}
-      >
-        Delete
-      </button>
-      <Link to="/">
-        <button className="notesList__buttonPrimary" type="button">
-          Back
-        </button>
-      </Link>
-      <ReactMarkdown>{note.markdown}</ReactMarkdown>
-    </>
+    <div className={styles.note}>
+      <div className={styles.note__wrapper}>
+        <h1 className={styles.note__title}>{note.title}</h1>
+        {note.tags.length > 0 && (
+          <ul className={styles.note__tagList}>
+            {note.tags.map((tag) => (
+              <li className={styles.note__tag} key={tag.id}>
+                {tag.label}
+              </li>
+            ))}
+          </ul>
+        )}
+        <ReactMarkdown className={styles.note__markdown}>
+          {note.markdown}
+        </ReactMarkdown>
+        <div className={styles.note__buttonWrapper}>
+          <Link to={`/${note.id}/edit`}>
+            <button className={styles.note__primary} type="button">
+              Edit
+            </button>
+          </Link>
+          <Link to="/">
+            <button
+              className={styles.note__delete}
+              onClick={() => {
+                onDelete(note.id);
+                // navigate("/");
+              }}
+              type="button"
+            >
+              Delete
+            </button>
+          </Link>
+          <Link to="/">
+            <button className="notesList__buttonPrimary" type="button">
+              Back
+            </button>
+          </Link>
+        </div>
+      </div>
+    </div>
   );
 };
 
